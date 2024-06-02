3ca11d96eac1195a7ea3f237ab6802974a8899a3
josej <josej@TABLET-0P3NLKVK>
2024-06-01 19:30:58 -0600

    ejemplo de java para el profe alfonsoooooo el grandeee

---
+++ src/playlist2/Playlist2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playlist2;
+
+/**
+ *
+ * @author josej
+ */
+public class Playlist2 {
+
+    public static void main(String[] args) {
+        musica p1=new musica(" El paso del gigante"," cumbias mexicanas "," 1987",3);
+        p1.ShowPlaylist();
+        
+    }
+}
